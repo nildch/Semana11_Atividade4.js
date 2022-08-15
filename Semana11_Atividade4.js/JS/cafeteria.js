d97e6d537894c249d1f6361f11971bb5eb7b9fe4
Nielen Jhose <96541549+nildch@users.noreply.github.com>
2022-08-15 15:22:07 -0300

    Add files via upload

---
+++ Semana11_Atividade4.js/JS/cafeteria.js
@@ -0,0 +1,30 @@
+let cafeteriaName = "IrishCoffee";
+
+let itens = [
+  {
+    nome: "Café Normal 200g.",
+    imagem: "./imagem/cafe.jpg",
+    preco: "25.0",
+    descricao: "Sabor inigualável de um café coado!",
+  },
+  {
+    nome: "Café Normal 100g.",
+    imagem: "./imagem/cafe.jpg",
+    preco: "50.0",
+    descricao: "Sabor inigualável de um café coado!",
+  },
+  {
+    nome: "X-cookie",
+    imagem: "./imagem/cookie.jpg",
+    preco: "25.0",
+    descricao: "Cookie com massa clássica com gotas de chocolate 40%",
+  },
+  {
+    nome: "Bolo Gourmet",
+    imagem: "./imagem/bolo.jpg",
+    preco: "25.0",
+    descricao: "Bolo de cenoura com calda de chocolate.",
+  },
+];
+
+export default itens;
---
+++ Semana11_Atividade4.js/JS/index.js
@@ -0,0 +1,39 @@
+import itens from "./cafeteria.js";
+
+function createFoodCard(food) {
+  let foodCard = `<div class="col-4">
+        <div class="card" style="width: 18rem;">
+            <img src="${food.imagem}" class="card-img-top" alt="${food.nome}">
+            <div class="card-body">
+                <h5 class="card-title">${food.nome}</h5>
+                <p class="card-text">
+                    ${food.descricao}
+                </p>
+            </div>
+        </div>
+    </div>`;
+  return foodCard;
+}
+
+function addFoodsCard() {
+  for (let food of itens) {
+    let foodCard = createFoodCard(food);
+    let cardDeck = document.getElementById("card-deck");
+    cardDeck.insertAdjacentHTML("beforeend", foodCard);
+  }
+}
+
+// Adicionar onclick no button: addFoodButton
+let addFoodButton = document.getElementById("addFoodButton");
+addFoodButton.onclick = (event) => {
+  // Recuperar o elemento do formulário pelo id.
+  let foodForm = document.getElementById("foodForm");
+
+  // Adicionar onsubmit no form: foodForm
+  foodForm.onsubmit = (e) => {
+    e.preventDefault();
+    //TODO: Manipular o formulário
+  };
+};
+
+addFoodsCard();
